7d147bef66f87fc462aadb93f0093a4cb4cbee5f
Vangelis <axokid318@gmail.com>
2021-03-27 17:44:08 +0000

    fix -finished vanilla tor

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from fastor.client import getClient
+from fastor.client.client import ClientException
 
 
 def main():
     client = getClient("vanilla")
-    client.attach()
-    res = client.query("http://facebook.com")
+
+    attached = False
+    while not attached:
+        try:
+            client.attach()
+            attached = True
+        except ClientException as e:
+            print(e)
+
+    worked = False
+    res = None
+    while not worked:
+        try:
+            res = client.query("http://facebook.com")
+            worked = True
+        except ClientException as e:
+            print(e)
 
     print(res)
-
     client.detach()
+    print("goodbye")
 
 
 if __name__ == "__main__":
